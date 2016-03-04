353637636ae97b2ee3bac40079546cf83fbd180d
root <root@go-server1.localdomain>
2016-03-04 14:29:46 +0000

    initial revision

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name:        "opswork-bootstrap"
+description  "Bootstrap recipe for dynamic chef recipe installation"
+version      "1.0.0"
+
+supports "ubuntu"
+
+recipe "opswork-bootstrap"
+
+
---
+++ recipes/dependencies.rb
@@ -0,0 +1,16 @@
+package chefdk
+
+node['opsworks-bootstrap']['cookbooks'].each do |cookbook|
+
+  git "/var/chef/cookbooks/#{cookbook['name']}"
+    repository cookbook['repository']
+    revision   cookbook['revision']
+    action     :sync
+  end
+
+  execute "berks-#{cookbook['name']}"
+    command 'berks vendor ../'
+    cwd     "/var/chef/cookbooks/#{cookbook['name']"
+  end
+
+end
